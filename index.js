09bfec006e75f20d38ff959f95df2dbf6c333f61
SamitMahadi <samithossainmahadi@gmail.com>
2023-02-05 14:26:33 -0500

    doneee

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'
 import * as dotenv from 'dotenv';
 import connectDB from './mongodb/connect.js';
 import imagoRoutes from './routes/imagoRoutes.js';
@@ -15,7 +15,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express()
-app.use(cors());
+app.use(cors())
 app.use (express.json({ limit: '50mb' }))
 
 
