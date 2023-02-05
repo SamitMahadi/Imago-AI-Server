22f9356e6775029f68f135bb9f3460ddba4abb33
SamitMahadi <samithossainmahadi@gmail.com>
2023-02-05 12:30:07 -0500

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 const app = express()
 app.use(cors());
-app.use (express.json())
+app.use (express.json({ limit: '50mb' }))
 
 
 app.use('/api/v1/post', postRoutes);
@@ -30,11 +30,12 @@ app.use('/api/v1/imago', imagoRoutes);
 app.get('/',async(req,res)=>{
     res.send('imago ai is runnig')
 })
-app.listen(port, ()=>{
-   try{
-    connectDB(process.env.MONGODB_URL)
-     console.log(`imago ai server running on ${port}`);
-   }catch{
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => console.log('Server started on port 5000'));
+  } catch (error) {
     console.log(error);
-   }
-})
+  }
+};
+startServer()
--- routes/imagoRoutes.js
+++ routes/imagoRoutes.js
@@ -6,8 +6,10 @@ dotenv.config()
 
 const router = express.Router()
 
+
+
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+   apiKey:process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration);
